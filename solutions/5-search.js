bee8795edeb34d65e42e737f288ecb4baa2d63a1
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:06 +0300

    completed 5 exercise

--- solutions/5-search.js
+++ solutions/5-search.js
@@ -1,5 +1,17 @@
 export default (document) => {
   // BEGIN
+  const title = document.querySelector('.content h1').textContent.trim();
+  const description = document.querySelector('.description').textContent.trim();
+  const articles = Array.from(document.querySelectorAll('.links > div'))
+    .map(article => ({
+      title: article.querySelector('h2 a').textContent.trim(),
+      description: article.querySelector('p').textContent.trim()
+    }));
 
+    return {
+      title,
+      description,
+      items: articles
+    };
   // END
 };
