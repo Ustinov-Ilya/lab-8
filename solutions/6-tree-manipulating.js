96a55825be5330d5cf816bb20bb47b9605ecca37
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:13 +0300

    completed 6 exercise

--- solutions/6-tree-manipulating.js
+++ solutions/6-tree-manipulating.js
@@ -1,3 +1,17 @@
 // BEGIN
+const prettify = (document) => {
+    const divs = document.querySelectorAll('div');
+    divs.forEach(div => {
+        const childNodes = Array.from(div.childNodes);
+        childNodes.forEach(node => {
+            if (node instanceof Text && node.textContent.trim() !== ''){
+                const pEl = document.createElement('p');
+                pEl.append(node.textContent);
+                node.replaceWith(pEl);
+            }
+        });
+    });
+}
 
+export default prettify
 // END
