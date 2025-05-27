b436ac6507e23a6d7111211ff7e79e1b321be296
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:53:52 +0300

    completed 3 exercise

--- solutions/3-dom.js
+++ solutions/3-dom.js
@@ -1,3 +1,12 @@
 // BEGIN
+const bodyContent = document.body.innerHTML;
 
+let lines = bodyContent
+    .split('\n')
+    .map(line => line.trim())
+    .filter(line => line.length > 0);
+
+const newBodyContent = lines.map(line => `<p>${line}</p>`).join('\n');
+
+document.body.innerHTML = newBodyContent;
 // END
