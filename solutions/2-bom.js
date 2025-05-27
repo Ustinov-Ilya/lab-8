40a8afa4d32d3d005741a418a534d4a0258fb6fb
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:53:45 +0300

    completed 2 exercise

--- solutions/2-bom.js
+++ solutions/2-bom.js
@@ -1,3 +1,12 @@
 // BEGIN
+const solution = (url) => {
+    window.location.href = url;
 
+    const codeName = navigator.appCodeName;
+    const version = navigator.appVersion;
+
+    return `${codeName}/${version} ${window.location.href = url}`;
+}
+
+export default solution
 // END
