c987e7ad18a288c5a3b85f2f608d53a5ab7eb881
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:53:59 +0300

    completed 4 exercise

--- solutions/4-dom-hierarchy.js
+++ solutions/4-dom-hierarchy.js
@@ -1,3 +1,11 @@
 // BEGIN
+const extractData = (DOM) => {
+    const body = DOM.querySelector('body');
+    return Array.from(body.childNodes)
+        .filter((str) => str.tagName === 'P')
+        .map(p => p.textContent.trim())
+        .filter(text => text.length > 0);
+}
 
+export default extractData
 // END
