51f053739181c0c731a9f3391b6b524b621d2af2
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:57 +0300

    completed 11 exercise

--- solutions/11/src/application.js
+++ solutions/11/src/application.js
@@ -2,6 +2,35 @@ import 'whatwg-fetch';
 
 export default () => {
   // BEGIN
-  
+  let elements = document.querySelectorAll('input[data-autocomplete]');
+
+  const processInput = async (e) => {
+    const target = e.target;
+    const endpoint = target.dataset.autocomplete;
+    const listIdentifier = target.dataset.autocompleteName;
+
+    const requestUrl = new URL(endpoint, window.location.origin);
+    requestUrl.searchParams.append('search', target.value);
+
+    const fetchedData = await fetch(requestUrl);
+    const parsedData = await fetchedData.json();
+
+    const resultContainer = document.querySelector(`ul[data-autocomplete-name="${listIdentifier}"]`);
+    resultContainer.innerHTML = '';
+
+    if (parsedData instanceof Array && parsedData.length) {
+      for (let entry of parsedData) {
+        const listItem = document.createElement('li');
+        listItem.textContent = entry;
+        resultContainer.appendChild(listItem);
+      }
+    } else {
+        resultContainer.innerHTML = '<li>Nothing</li>';
+    }
+  };
+
+  elements.forEach(element => {
+      element.addEventListener('input', processInput);
+  });
   // END
 };
