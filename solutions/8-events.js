67032db61ede215a004e994abcc0244b298412ec
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:28 +0300

    completed 8 exercise

--- solutions/8-events.js
+++ solutions/8-events.js
@@ -1,5 +1,16 @@
 export default () => {
   // BEGIN
+  const button = document.getElementById('alert-generator');
+  const alerts = document.querySelector('.alerts');
 
+  let countAlerts = 1;
+
+  button.addEventListener('click', () => {
+    const newAlert = document.createElement('div');
+    newAlert.classList.add('alert', 'alert-primary');
+    newAlert.textContent = `Alert ${countAlerts}`;
+    alerts.insertBefore(newAlert, alerts.firstChild);
+    countAlerts += 1;
+  });
   // END
 };
