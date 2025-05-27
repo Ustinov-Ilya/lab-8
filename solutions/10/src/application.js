2a1b3b99c8bc1c17399e965fbddc63bc51e05e33
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:46 +0300

    completed 10 exercise

--- solutions/10/src/application.js
+++ solutions/10/src/application.js
@@ -1,5 +1,20 @@
 import escapeHtml from 'escape-html';
 
 // BEGIN
+export default () => {
+    const form = document.querySelector('form');
+    if (!form) return;
 
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const container = document.createElement('div');
+        container.innerHTML = `
+            <p>Feedback has been sent</p>
+            <div>Email: ${escapeHtml(form.elements.email.value)}</div>
+            <div>Name: ${escapeHtml(form.elements.name.value)}</div>
+            <div>Comment: ${escapeHtml(form.elements.comment.value)}</div>
+        `;
+        form.replaceWith(container);
+    });
+}
 // END
