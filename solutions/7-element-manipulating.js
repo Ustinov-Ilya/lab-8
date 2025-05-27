4070a04e231ef010f0beaf6139061ee7f2141fd4
Ustinov Ilya <ustinov-bis231@yandex.ru>
2025-05-27 16:54:20 +0300

    completed 7 exercise

--- solutions/7-element-manipulating.js
+++ solutions/7-element-manipulating.js
@@ -1,5 +1,14 @@
 import camelCase from 'lodash/camelCase';
 
 // BEGIN
+const normalize = (document) => {
+    const elements = document.body.getElementsByTagName('*');
+    Array.from(elements).forEach(element => {
+        const classes = element.className.split(' ');
+        const normalizedClasses = classes.map(className => camelCase(className));
+        element.className =normalizedClasses.join(' ');
+    });
+}
 
+export default normalize
 // END
